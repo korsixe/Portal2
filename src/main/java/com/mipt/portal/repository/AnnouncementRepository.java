23b23811079a9276419c7b16c29585b266dc0469
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2026-03-24 17:47:57 +0300

    feature: обновил AnnouncmentRepo и добавил Customs для расширения CRUD

--- src/main/java/com/mipt/portal/repository/AnnouncementRepository.java
+++ src/main/java/com/mipt/portal/repository/AnnouncementRepository.java
@@ -2,15 +2,12 @@ package com.mipt.portal.repository;
 
 import com.mipt.portal.entity.Announcement;
 import com.mipt.portal.enums.AdStatus;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, JpaSpecificationExecutor<Announcement> {
-  @EntityGraph(attributePaths = "photoUrls")
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, CustomAnnouncementRepository {
   List<Announcement> findAllByStatus(AdStatus status);
 }
