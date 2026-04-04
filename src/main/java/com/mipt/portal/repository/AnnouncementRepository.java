780faa5824e7adcb448c13098703ca2f901443a4
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2026-04-04 14:57:06 +0300

    feat: создал Фильтр для добавления и update AnnRepository

--- src/main/java/com/mipt/portal/repository/AnnouncementRepository.java
+++ src/main/java/com/mipt/portal/repository/AnnouncementRepository.java
@@ -2,12 +2,21 @@ package com.mipt.portal.repository;
 
 import com.mipt.portal.entity.Announcement;
 import com.mipt.portal.enums.AdStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, CustomAnnouncementRepository {
-  List<Announcement> findAllByStatus(AdStatus status);
+    List<Announcement> findAllByStatus(AdStatus status);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Announcement a WHERE a.id = :id")
+    Optional<Announcement> findByIdWithLock(@Param("id") Long id);
 }
