03e146704dc6583bf16140fb013d720964d3607b
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2026-04-04 16:41:32 +0300

    feat: исправил ошибку новичка

--- src/main/java/com/mipt/portal/entity/Booking.java
+++ src/main/java/com/mipt/portal/entity/Booking.java
@@ -1,13 +1,17 @@
 package com.mipt.portal.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
+import lombok.*;
 import java.time.Instant;
+import java.util.Objects;
 
-@Data
 @Entity
 @Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Booking {
 
     @Id
@@ -22,4 +26,17 @@ public class Booking {
 
     @Column(name = "created_at", updatable = false)
     private Instant createdAt = Instant.now();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(id, booking.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
