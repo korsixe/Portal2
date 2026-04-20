04b850bc3682fa0559856853a91539a5d3b18239
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2026-04-20 22:36:30 +0300

    chore: extract frontend

--- src/main/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders portal header', () => {
-  render(<App />);
-  const header = screen.getByText(/portal/i);
-  expect(header).toBeInTheDocument();
-});
